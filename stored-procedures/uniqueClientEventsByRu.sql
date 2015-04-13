c54b8c6aff3d4dd58346a86b70aabe7220702758
Tim Markello <timmarkello@gmail.com>
2015-04-13 16:08:56 -0500

    - remove the specific db + add new parameter @QualifyingSac    > Parameter forces client to have attended at least 1 event from the list of sacs provided in the parameter, if the parameter has a value + add order by for sanity

--- stored-procedures/uniqueClientEventsByRu.sql
+++ stored-procedures/uniqueClientEventsByRu.sql
@@ -1,9 +1,13 @@
-use wood
+/*
+exec uniqueClientEventsByRu 3600, '480,1500,1501,2201,2202,2203,2204,2205,460', '10/1/2000', '4/30/2015'
+*/
+
 if OBJECT_ID ('uniqueClientEventsByRu') is not null 
 	drop procedure uniqueClientEventsByRu
 go
 create procedure uniqueClientEventsByRu
 	@EventRu as nvarchar(max),
+	@QualifyingSac as nvarchar(max),
 	@StartDate as DateTime,
 	@EndDate as DateTime
 as 
@@ -12,9 +16,13 @@ as
 	where RU In (
 		select data from dbo.Split(@EventRu, ',')
 	)
+	and (@QualifyingSac is null or SAC in (
+		select data from dbo.Split(@QualifyingSac, ',')
+	))
 	and EventDate between @startdate and @enddate
         and Attendance in ('1', '2', '3')
         and Recipient in ('1', '4', '5', '6')
         and Location not in ('8')
         and ClientDur > '0'
+    order by ClientID
 go
